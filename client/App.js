e2f96fc6f2f51b42a4a728dac979d53439a15f04
Dan Actor <danltactor@gmail.com>
2018-07-01 18:09:25 -0600

    basic app.js setup

--- client/App.js
+++ client/App.js
@@ -1,23 +1,44 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Nativerouter, Switch, Route } from 'react-router-native';
+import { StackNavigator } from 'react-navigation';
+import { Provider } from 'react-redux';
+import store from './store';
+import { Root } from 'native-base';
+import Home from './components/Home';
 
-export default class App extends React.Component {
+class App extends React.Component {
+
   render() {
+    console.disableYellowBox = true;
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
-      </View>
+      <Provider store={store}>
+        <NativeRouter>
+          <Switch>
+            <Route exact path='/' component={Home} />
+          </Switch>
+        </NativeRouter>
+      </Provider>
     );
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const AppNav = StackNavigator(
+  {
+    Settings: { screen: Settings },
+  }
+);
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#fff',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+// });
+
+export default () =>
+  <Root>
+    <App />
+  </Root>;
+
